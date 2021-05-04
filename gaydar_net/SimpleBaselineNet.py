0c233e891a275932cae7cb513e8143ce01a884e3
michael5511b <michael5511b@github.com>
2021-05-03 22:28:37 -0400

    gaydar_net is now online lmao.

---
+++ gaydar_net/SimpleBaselineNet.py
@@ -0,0 +1,44 @@
+import torch
+import torch.nn as nn
+import torchvision.models as models
+
+class SimpleBaselineNet(nn.Module):
+    """
+    A vanilla regression model which takes facial images as input and predicts ones' height
+    """
+    def __init__(self):
+        super().__init__()
+        self.features = nn.Sequential(*list(models.vgg16(pretrained=True).children())[:-1])
+        self.regressor = nn.Sequential(
+            # (N, 512, 1, 1)
+            nn.MaxPool2d((7, 7)),
+            # (N, 512)
+            nn.Flatten(),
+            # fc1
+            #nn.Linear(25088, 4096),
+            nn.Linear(512, 128),
+            nn.BatchNorm1d(128),
+            nn.ReLU(),
+            nn.Dropout(0.4),
+            # fc2
+            #nn.Linear(4096, 1024),
+            nn.Linear(128, 10),
+            nn.BatchNorm1d(10),
+            nn.ReLU(),
+            nn.Dropout(0.4),
+            # fc3
+            nn.Linear(10, 2),
+        )
+
+    def forward(self, x):
+        """
+        :param x: input image in shape of (N, 3, 224, 224)
+        :return out: regression predictions of height (N, )
+        """
+        x = self.features(x)
+        x = self.regressor(x).squeeze()
+
+        return x
+
+
+
---
+++ height_net/simple_baseline_experiment_runner.py
@@ -0,0 +1,64 @@
+from SimpleBaselineNet import SimpleBaselineNet
+from experiment_runner_base import ExperimentRunnerBase
+from celeba_dataset import CelebADataset
+
+import torch
+import torch.nn as nn
+from torchvision import transforms
+
+class SimpleBaselineExperimentRunner(ExperimentRunnerBase):
+    """
+    Sets up the Simple Baseline model for training. This class is specifically responsible for creating the model and optimizing it.
+    """
+    def __init__(self, batch_size, num_epochs, model_path,
+                 num_data_loader_workers, lr, log_validation):
+
+        ############ 2.3 TODO: set up transform
+        transform = transforms.Compose([
+            transforms.Resize(256),
+            transforms.CenterCrop(224),
+            transforms.ToTensor(),
+            transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
+        ])
+        ############
+
+        train_dataset = CelebADataset(image_dir="../data/img_align_celeba",
+            annotation_txt_path="../data/final_data.txt",
+            data_partition_path="../data/list_eval_partition.txt",
+            train_val_flag="train",
+            transform=transform,
+        )
+
+        val_dataset = CelebADataset(image_dir="../data/img_align_celeba",
+            annotation_txt_path="../data/final_data.txt",
+            data_partition_path="../data/list_eval_partition.txt",
+            train_val_flag="val",
+            transform=transform,
+        )
+
+        model = SimpleBaselineNet()
+
+        super().__init__(train_dataset, val_dataset, model, batch_size, num_epochs, model_path, num_data_loader_workers, log_validation)
+
+        ############ 2.5 TODO: set up optimizer
+        # no need to train GoogLeNet parameters
+        self.optimizer = torch.optim.SGD(
+            self._model.parameters(),
+            lr = lr,
+            momentum=0.9,
+            #weight_decay=1e-5,
+        )
+        ############
+
+
+    def _optimize(self, predicted_height, ground_truth_height):
+        ############ 2.7 TODO: compute the loss, run back propagation, take optimization step.
+        criterion = nn.MSELoss(reduction='mean').cuda()
+        loss = criterion(predicted_height, ground_truth_height)
+
+        self.optimizer.zero_grad()
+        loss.backward()
+        self.optimizer.step()
+
+        return loss
+        ############
